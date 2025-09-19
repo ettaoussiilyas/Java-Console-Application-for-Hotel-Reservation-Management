69dfec1bec36b6a0234e452a0a065a3bbbc1cca9
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-09-19 10:19:36 +0100

    Enhanced `Hotel` class: added deep copy constructor, input validation for setters, `equals` and `hashCode` overrides, and `totalRooms` property.

--- src/entity/Hotel.java
+++ src/entity/Hotel.java
@@ -3,69 +3,98 @@ package entity;
 import java.util.UUID;
 
 public class Hotel {
-
+    private final String hotelId;
     private String name;
     private String address;
-    private String hotelId;
     private int availableRooms;
+    private int totalRooms;
     private float rating;
     private double price;
 
-
-    public Hotel(String name, String address, int availableRooms, float rating, double price) {
+    public Hotel(String name, String address, int totalRooms, float rating, double price) {
+        this.hotelId = UUID.randomUUID().toString();
         this.name = name;
         this.address = address;
-        this.hotelId = UUID.randomUUID().toString();
-        this.availableRooms = availableRooms;
+        this.totalRooms = totalRooms;
+        this.availableRooms = totalRooms;
         this.rating = rating;
         this.price = price;
     }
 
-    public String getAddress() {
-        return address;
+    public Hotel(Hotel other) {
+        this.hotelId = other.hotelId;
+        this.name = other.name;
+        this.address = other.address;
+        this.totalRooms = other.totalRooms;
+        this.availableRooms = other.availableRooms;
+        this.rating = other.rating;
+        this.price = other.price;
+    }
+
+    public String getHotelId() {
+        return hotelId;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getIdHotel() {
-        return hotelId;
-    }
-
-    public float getRating() {
-        return rating;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getAvailableRooms() {
-        return availableRooms;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-    public void setIdHotel(String idHotel) {
-        this.hotelId = idHotel;
+    public int getAvailableRooms() {
+        return availableRooms;
     }
 
     public void setAvailableRooms(int availableRooms) {
-        this.availableRooms = availableRooms;
+        if (availableRooms >= 0 && availableRooms <= totalRooms) {
+            this.availableRooms = availableRooms;
+        }
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public float getRating() {
+        return rating;
     }
 
     public void setRating(float rating) {
-        this.rating = rating;
+        if (rating >= 0 && rating <= 5) {
+            this.rating = rating;
+        }
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        if (price > 0) {
+            this.price = price;
+        }
+    }
+
+    @Override
+    public boolean equals(Object objectInstant) {
+        if (this == objectInstant) return true;
+        if (!(objectInstant instanceof Hotel)) return false;
+        Hotel hotel = (Hotel) objectInstant;
+        return hotelId.equals(hotel.hotelId);
+    }
+
+    @Override
+    public int hashCode() {
+        return hotelId.hashCode();
     }
 }
